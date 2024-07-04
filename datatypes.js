31e279befd29d12ae674c3a772103e7f396b4144
AnsumanGeek123 <140577013+AnsumanGeek123@users.noreply.github.com>
2024-07-05 03:04:01 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+// Primitive 
+// -> string, Number, Boolean, null, undefined, symbol, BigInt
+ 
+// Refrence Type (Non primitive)
+// Arrays, Objects, Functions 
+
+const score = 100;
+
+// JavaScript is a dynamically typed language, 
+// which means that data types of variables
+//  are determined by the value they hold at
+// runtime and can change throughout the program as
+//  we assign different values to them.
+
+const heros = ["shaktiman", "bulu", "peta"];
+
+let object={
+    name: "ANSUMAN",
+    roll: "121mm01651",
+} 
+
+console.log(typeof heros); // objects as output
+console.log(typeof object);
