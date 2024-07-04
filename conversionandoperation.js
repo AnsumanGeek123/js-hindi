5934429c536a71ec7e4fedf3927a7c833a96e26f
AnsumanGeek123 <140577013+AnsumanGeek123@users.noreply.github.com>
2024-07-05 03:03:24 +0530

    Add files via upload

---
+++ conversionandoperation.js
@@ -0,0 +1,10 @@
+let str = "33";
+let num = Number(str);
+console.log(typeof (num)); // Op is number so "33" = 33
+
+// If anything is not possible then it returns NaN
+let string = "ansuman";
+let convert = Number(string);
+console.log(convert); // Op is NaN
+
+// True - > 1; False-> 0
